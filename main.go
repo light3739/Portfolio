48815487b63542f071e22c55cbde58081067de66
light3739 <maxikpetlitskii@gmail.com>
2024-01-12 23:44:25 +0100

    Adding Calculator section

--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import (
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	// Read the contents of index.html and header.html from the templates folder
-	tmpl, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/tools.html", "templates/about.html")
+	tmpl, err := template.ParseFiles(
+		"templates/index.html", "templates/header.html",
+		"templates/tools.html", "templates/about.html",
+		"templates/calculator.html",
+	)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -18,10 +22,12 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
 }
 
 func main() {
-	http.HandleFunc("/", indexHandler)
 
+	http.HandleFunc("/", indexHandler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.ListenAndServe(":8080", nil)
 }
