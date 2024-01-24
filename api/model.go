82ef4f54e9ab1feb3d132d72707b208e1019f512
light3739 <maxikpetlitskii@gmail.com>
2024-01-24 19:19:16 +0100

    Formatting middleware.go and model.go with fmt linter. Added in model.go SMTPConfig struct.

--- api/model.go
+++ api/model.go
@@ -6,3 +6,11 @@ type ContactForm struct {
 	Email     string
 	Message   string
 }
+
+type SMTPConfig struct {
+	Host           string
+	Port           string
+	Email          string
+	Password       string
+	RecipientEmail string
+}
